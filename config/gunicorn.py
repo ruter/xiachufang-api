a548d8dd6f848fe36277dfaad46a685958045cac
Ruter <ruter.lv@gmail.com>
2017-12-22 13:51:55 +0800

    [fix] create dir log if path not exist

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -2,7 +2,10 @@ import os
 import multiprocessing
 
 
-CWD = os.getcwd()
+LOG_PATH = os.path.join(os.getcwd(), 'log/')
+
+if not os.path.exists(LOG_PATH):
+    os.makedirs(LOG_PATH)
 
 bind = '127.0.0.1:5000'
 graceful_timeout = 30
@@ -12,5 +15,5 @@ workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
 
 loglevel = 'info'
-accesslog = os.path.join(CWD, 'log/gunicorn_access.log') #访问日志文件
-errorlog = os.path.join(CWD, 'log/gunicorn_error.log')   #错误日志文件
+accesslog = os.path.join(LOG_PATH, 'gunicorn_access.log') #访问日志文件
+errorlog = os.path.join(LOG_PATH, 'gunicorn_error.log')   #错误日志文件
