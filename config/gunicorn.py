d9d625ceff7bd77936ab3638249147237d63cb85
Ruter <ruter.lv@gmail.com>
2017-12-22 14:12:53 +0800

    [update] change gunicorn config

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -7,7 +7,7 @@ LOG_PATH = os.path.join(os.getcwd(), 'log/')
 if not os.path.exists(LOG_PATH):
     os.makedirs(LOG_PATH)
 
-bind = '127.0.0.1:5000'
+bind = '0.0.0.0:5000'
 graceful_timeout = 30
 preload_app = True
 worker_class = 'gevent'
@@ -15,5 +15,5 @@ workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
 
 loglevel = 'info'
-accesslog = os.path.join(LOG_PATH, 'gunicorn_access.log') #访问日志文件
-errorlog = os.path.join(LOG_PATH, 'gunicorn_error.log')   #错误日志文件
+accesslog = os.path.join(LOG_PATH, 'access.log') #访问日志文件
+errorlog = os.path.join(LOG_PATH, 'error.log')   #错误日志文件
